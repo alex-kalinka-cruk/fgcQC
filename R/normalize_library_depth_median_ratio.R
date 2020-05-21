82fa706ab78853ecaa6b04c0c99ccf1638dfabd2
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-05-22 00:00:42 +0100

    norm med ratio first pass.

--- R/normalize_library_depth_median_ratio.R
+++ R/normalize_library_depth_median_ratio.R
@@ -1,3 +1,9 @@
+.geom_mean <- function(...){
+  x <- c(unlist(list(...)))
+  return(exp(sum(log(x[x > 0]), na.rm=T) / length(x)))
+}
+
+
 #' normalize_library_depth_median_ratio
 #'
 #' Normalizes library depth from a counts file using the median-ratio method [1].
@@ -5,8 +11,19 @@
 #' @param counts A data frame of counts for each sample in the study (samples as columns, gRNAs as rows).
 #'
 #' @return A data frame in which the sample columns are library-depth normalized.
-#' @importFrom dplyr
+#' @importFrom dplyr mutate summarise select
+#' @importFrom purrr pmap_dbl
 #' @export
 normalize_library_depth_median_ratio <- function(counts){
-
+  tryCatch({
+    size_factors <- counts %>%
+      dplyr::mutate(geom_mean = purrr::pmap_dbl(
+        .l = dplyr::select(., -sgRNA, -gene),
+        .f = .geom_mean
+      )) %>%
+      dplyr::summarise()
+  },
+  error = function(e) stop(paste("unable to calculate median ratio normalization:",e))
+  )
+  return(size_factors)
 }
